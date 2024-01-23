8bf9a975a5af280b0dfb52d2dc1a2f2bc30927ed
RAximberdi <r.raxonberdiyev@newuu.uz>
2024-01-23 23:52:23 +0500

    updated

--- src/cpp/task_1.cpp
+++ src/cpp/task_1.cpp
@@ -4,6 +4,27 @@
  * Name:
  */
 
-class Problem1 {
+#include <vector>
+using namespace std;
+
+class Solution {
 public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum < target || -sum > target || (sum + target) % 2 == 1) {
+            return 0;
+        }
+        int newTarget = (sum + target) / 2;
+        vector<int> dp(newTarget + 1, 0);
+        dp[0] = 1;
+        for (int num : nums) {
+            for (int i = newTarget; i >= num; i--) {
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp[newTarget];
+    }
 };
